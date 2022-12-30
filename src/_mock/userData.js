5171c5f6e37980feb97fa14626ef03d0046674c9
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-12-30 23:38:10 +0900

    data : 유더 더미 데이터 추가

---
+++ src/_mock/userData.js
@@ -0,0 +1,26 @@
+export const userData = [
+  {
+    id: 1,
+    nickname: "이화연 일반 유저",
+    username: "0000",
+    isBooth: false,
+    isTf: false,
+    boothId: null,
+  },
+  {
+    id: 2,
+    nickname: " 부스 관리자",
+    username: "1111",
+    isBooth: true,
+    isTf: false,
+    boothId: 1,
+  },
+  {
+    id: 3,
+    nickname: "TF 유저",
+    username: "2222",
+    isBooth: false,
+    isTf: true,
+    boothId: null,
+  },
+];
